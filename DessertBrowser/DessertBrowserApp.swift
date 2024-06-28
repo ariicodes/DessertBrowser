e9e2faf9b66d3feb9399f1aaa7f3cf23a2844e5c
Ariela Israel <arielaisrael12@gmail.com>
2024-06-28 02:54:44 -0400

    Initial Commit

---
+++ DessertBrowser/DessertBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertBrowserApp.swift
+//  DessertBrowser
+//
+//  Created by Ariela Israel on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
